b81e48512becaf367ad3e5e8e86b634756715282
lafwan <oussamaoussimo@gmail.com>
2023-03-27 17:58:45 -0700

    hope

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+/**
+ * swap- a function that swaps the values of two integers.
+ * @a:input1
+ * @b:input2
+ * Return: none
+ */
+void swap_int(int *a, int *b)
+{
+	int swap;
+	swap = *a;
+	*a = *b;
+	*b = swap;
+}
