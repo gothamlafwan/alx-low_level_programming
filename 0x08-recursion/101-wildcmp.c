e3aeb5e8d473d86dd5933f0f47b94c3cb6c5e6bd
lafwan <oussamaoussimo@gmail.com>
2023-04-05 18:18:54 -0700

    re

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,12 +1,31 @@
+#include "main.h"
+
 /**
- * wildcmp - is_palindrome
- * @s1: integer paramtr
- * @s2: var
- * Return: true or false
+ * wildcmp - Compare strings
+ * @s1: pointer to string params
+ * @s2: pointer to string params
+ * Return: 0
  */
+
 int wildcmp(char *s1, char *s2)
 {
-	(void)s1;
-	(void)s2;
-	return (1);
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
+
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
 }
